7a7f0757bd2aaa107c3e4ba42fe0312419a9720a
Generic626 <56214455+Generic626@users.noreply.github.com>
2024-03-01 20:06:35 +0800

    fronend: after merge modified the CheckoutPage to show form when isSuccess equals false, modified the trash can style in CheckoutProduct.jsx

--- frontend/petopia-shop/src/components/CheckoutSuccess.jsx
+++ frontend/petopia-shop/src/components/CheckoutSuccess.jsx
@@ -3,14 +3,12 @@ import success_pic from "../assets/success.png";
 
 const CheckoutSuccessPage = () => {
   return (
-      <div className=" m-auto w-[400px] p-5 justify-center flex flex-col">
-        <img
-        src={success_pic}
-        alt="success"
-        className="h-[280px] w-[350px]"
-      />
-      <h1 className="text-[30px] font-semibold mb-4 text-center p-5">Purchase Completed</h1>
-      </div>
+    <div className=" m-auto w-[400px] p-5 justify-center flex flex-col">
+      <img src={success_pic} alt="success" className="h-[280px] w-[350px]" />
+      <h1 className="text-[30px] font-semibold mb-4 text-center p-5">
+        Purchase Completed
+      </h1>
+    </div>
   );
 };
 
