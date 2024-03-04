4465bd1bc79f4227650af44f804d2bd1fe327a73
CasparFung <93065595+CasparFung@users.noreply.github.com>
2024-03-05 05:10:13 +0800

    Add files via upload

---
+++ frontend/petopia-shop/src/pages/ProductDetailPage.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import NavbarLayout from "../layout/NavbarLayout";
+import ProductDetail from "../components/ProductDetail";
+import Product1 from "../assets/product_1.webp";
+
+const ProductDetailPage = () => {
+  const [quantity, setQuantity] = useState(1);
+
+  const handleQuantityChange = (event) => {
+    setQuantity(parseInt(event.target.value));
+  };
+
+  const handleAddToCart = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <NavbarLayout>
+      <div>
+        <ProductDetail
+            stock={10}
+            productName="Food 1"
+            src={Product1}
+            alt="Food 1"
+            description="Tuna: Excellent choice for a high quality protein and low-fat fish. Rich in Omega-3 (EPA & DHA), helps neural, brain, retinal and foetal development with addition to improve skin and coat condition. Mousse texture: Suitable for cats & dogs that have difficulty in chewing solid food. And it is convenient to mix and feed with dry food and supplement. Halal certified"
+            quantity={quantity}
+            handleQuantityChange={handleQuantityChange}
+            handleAddToCart={handleAddToCart}
+        />
+      </div>
+    </NavbarLayout>
+  );
+};
+
+export default ProductDetailPage;
