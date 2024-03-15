cc190c18e6c9bd26c7e1a73d5872e9c2969ece20
Generic626 <56214455+Generic626@users.noreply.github.com>
2024-03-15 22:07:35 +0800

    frontend: small fix updated authentication-header to read from session instead of localStorage

--- frontend/petopia-shop/src/functions/authentication-header.js
+++ frontend/petopia-shop/src/functions/authentication-header.js
@@ -3,7 +3,7 @@ const requestHeader = (contentType) => {
     ...contentType,
     Authorization:
       "Bearer " +
-      (localStorage.getItem("token") ||
+      (sessionStorage.getItem("token") ||
         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6ImFkbWluXzEyMyIsIm5hbWVpZCI6ImI2YmFkZTgyLTdiZDctNDZiYS1iZTc4LTFlZThhOTMyM2VkMyIsInJvbGUiOiJBZG1pbiIsIm5iZiI6MTcwOTgyNDE5NCwiZXhwIjoxNzQxMzYwMTk0LCJpYXQiOjE3MDk4MjQxOTQsImlzcyI6InBldG9waWEiLCJhdWQiOiJwZXRvcGlhIn0.Kob1n-Ecep-Y7xUPiv9bVZGhd2I5sB1_qx9UJq9ppa8"),
   };
 };
